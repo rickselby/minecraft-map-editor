b8daff87ad7c9c32e61b76e4b7729b707445e611
Rick Selby <rick@selby-family.co.uk>
2015-12-29 16:09:32 +0000

    Add flower pot block entity data

--- src/Block/FlowerPot.php
+++ src/Block/FlowerPot.php
@@ -4,13 +4,23 @@ namespace MinecraftMapEditor\Block;
 
 class FlowerPot extends \MinecraftMapEditor\Block
 {
-    use Traits\Create;
+    use Traits\Create, Traits\EntityData;
 
-    public function __construct()
+    /**
+     * A flower pot, optionally containing a flower
+     *
+     * @param \MinecraftMapEditor\Block $contains The flower the block contains
+     */
+    public function __construct($contains = null)
     {
         // This is going to define the plant in the tile entity data
         // - which is to be done...
 
         $this->setBlockIDAndDataFor(Ref::FLOWER_POT);
+
+        $this->initEntityData('FlowerPot');
+
+        $this->entityData->addChild(\Nbt\Tag::tagString('Item', $contains ? Names::$list[$contains->ref] : ''));
+        $this->entityData->addChild(\Nbt\Tag::tagInt('Data', $contains ? $contains->data : 0));
     }
 }
