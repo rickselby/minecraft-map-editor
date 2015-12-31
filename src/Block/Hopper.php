182498e250ced3f2a493173e1873e4baf881df19
Rick Selby <rick@selby-family.co.uk>
2015-12-31 11:28:27 +0000

    Add hopper block entity data.

--- src/Block/Hopper.php
+++ src/Block/Hopper.php
@@ -18,12 +18,18 @@ class Hopper extends \MinecraftMapEditor\Block
     /**
      * Get a hopper, with the output in the given direction.
      *
-     * @param int $output Output direction; one of the OUTPUT_ class constants
-     * @param int $active [Optional] Either Hopper::ACTIVE or Hopper::DISABLED
+     * @param int                         $output           Output direction; one of the OUTPUT_ class constants
+     * @param \MinecraftMapEditor\Stack[] $items            Items in the dropper, with pre-set slots (0-8).
+     *                                                      0 is the top-left corner.
+     * @param int                         $active           [Optional] Either Hopper:INACTIVE or Hopper::ACTIVE
+     * @param string                      $customName       Custom name for the chest, appears in GUI
+     * @param string                      $lock             Lock the dropper so it can only be opened if the player
+     *                                                      is holding an item whose name matches this string
+     * @param int                         $transferCooldown Time to next transfer in game ticks.
      *
      * @throws \Exception
      */
-    public function __construct($output, $active = self::ACTIVE)
+    public function __construct($output, $items = [], $active = self::ACTIVE, $customName = null, $lock = null, $transferCooldown = 0)
     {
         $this->setBlockIDFor(Ref::HOPPER);
 
@@ -31,5 +37,11 @@ class Hopper extends \MinecraftMapEditor\Block
         $this->checkInList($active, [self::ACTIVE, self::DISABLED], 'Invalid active reference for hopper');
 
         $this->setBlockData($output | $active);
+
+        $this->initEntityData('Hopper');
+        $this->addItemStacks($items);
+        $this->setCustomName($customName);
+        $this->setLock($lock);
+        $this->entityData->addChild(\Nbt\Tag::tagInt('TransferCooldown', $transferCooldown));
     }
 }
